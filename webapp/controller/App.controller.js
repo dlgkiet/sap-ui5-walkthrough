558963399fc557c218b91a48ebfc28dd6c78b70a
dlgkiet <giakiet20012004@gmail.com>
2025-08-25 11:45:02 +0700

    Step 5: Controllers

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict"
+
+    return Controller.extend("sap.ui.demo.walkthrough.App",{
+        onShowHello: () => {
+            alert("Hello there!")
+        }
+    })
+})
